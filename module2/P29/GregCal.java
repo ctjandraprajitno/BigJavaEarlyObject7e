10d5302746dff54fc6c9f7e2ba60b125dec62eda
ctjandraprajitno <ctjandraprajitno@gmail.com>
2025-10-02 23:36:55 -0400

    updated P2.9

--- module2/P29/GregCal.java
+++ module2/P29/GregCal.java
@@ -9,16 +9,18 @@ public class GregCal {
   public static void main(String[] args) {
     GregorianCalendar cal = new GregorianCalendar();
     
-    System.out.println("Today is: " + cal.getTime());
+    System.out.println("Today is: " + cal.get(GregorianCalendar.MONTH) + "/" + cal.get(GregorianCalendar.DATE) + "/" + cal.get(GregorianCalendar.YEAR));
 
     cal.add(GregorianCalendar.DAY_OF_YEAR, 100);
 
-    System.out.println("100 days from today is: " + cal.getTime());
+    System.out.println("100 days from today is: " + cal.get(GregorianCalendar.DAY_OF_WEEK));
 
     GregorianCalendar birthday = new GregorianCalendar(2000, GregorianCalendar.FEBRUARY, 29);
 
+    System.out.println("My birthday is on a: " + birthday.get(GregorianCalendar.DAY_OF_WEEK));
+
     birthday.add (GregorianCalendar.DAY_OF_YEAR, 10000);
     
-    System.out.println("10,000 days from my birthday is: " + birthday.getTime());
+    System.out.println("10,000 days from my birthday is: " + birthday.get(GregorianCalendar.MONTH) + "/" + birthday.get(GregorianCalendar.DATE) + "/" + birthday.get(GregorianCalendar.YEAR));
   }
 }
