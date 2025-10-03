5c5eced5705a6752d1755a1a9252a62259778cd7
ctjandraprajitno <ctjandraprajitno@gmail.com>
2025-10-02 23:30:04 -0400

    updated P2.9 to be more specific

--- module2/P29/GregCal.java
+++ module2/P29/GregCal.java
@@ -3,21 +3,22 @@
 // • The weekday of your birthday.
 // • The date that is 10,000 days from your birthday.
 
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class GregCal {
   public static void main(String[] args) {
-    Calendar cal = new GregorianCalendar();
+    GregorianCalendar cal = new GregorianCalendar();
     
     System.out.println("Today is: " + cal.getTime());
-    cal.add(Calendar.DAY_OF_YEAR, 100);
+
+    cal.add(GregorianCalendar.DAY_OF_YEAR, 100);
+
     System.out.println("100 days from today is: " + cal.getTime());
 
-    Calendar birthday = new GregorianCalendar(2000, Calendar.FEBRUARY, 29);
-    birthday.add (Calendar.DAY_OF_YEAR, 10000);
-    System.out.println("10,000 days from my birthday is: " + birthday.getTime());
+    GregorianCalendar birthday = new GregorianCalendar(2000, GregorianCalendar.FEBRUARY, 29);
 
-    System.out.println("Today is a " + cal.getTime());
+    birthday.add (GregorianCalendar.DAY_OF_YEAR, 10000);
+    
+    System.out.println("10,000 days from my birthday is: " + birthday.getTime());
   }
 }
